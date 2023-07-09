7bae252919b4ce3a15e4d95efbd0f2752cf3da8c
AndreD4 <andremai629@gmail.com>
2023-07-09 19:39:15 +0900

    Wrap in if Statment ShouldMove bool

--- Source/CryptRaiders/Mover.cpp
+++ Source/CryptRaiders/Mover.cpp
@@ -1,6 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Mover.h"
 
 #include "Math/UnrealMathUtility.h"
@@ -8,35 +7,32 @@
 // Sets default values for this component's properties
 UMover::UMover()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+  // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+  // off to improve performance if you don't need them.
+  PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+  // ...
 }
-
 
 // Called when the game starts
 void UMover::BeginPlay()
 {
-	Super::BeginPlay();
+  Super::BeginPlay();
 
-	OriginalLocation = GetOwner()->GetActorLocation();
-	
+  OriginalLocation = GetOwner()->GetActorLocation();
 }
-
 
 // Called every frame
-void UMover::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UMover::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-  
-  FVector CunrrentLocation = GetOwner()->GetActorLocation();
-  FVector TargetLocation = OriginalLocation + MoveOffset;
-  float Speed = FVector:: Distance(OriginalLocation, TargetLocation) / MoveTime;
+  Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+  if (ShouldMove)
+  {
+    FVector CunrrentLocation = GetOwner()->GetActorLocation();
+    FVector TargetLocation = OriginalLocation + MoveOffset;
+    float Speed = FVector::Distance(OriginalLocation, TargetLocation) / MoveTime;
 
-  FVector NewLocation = FMath::VInterpConstantTo(CunrrentLocation, TargetLocation, DeltaTime, Speed);
-  GetOwner()-> SetActorLocation(NewLocation);
-
+    FVector NewLocation = FMath::VInterpConstantTo(CunrrentLocation, TargetLocation, DeltaTime, Speed);
+    GetOwner()->SetActorLocation(NewLocation);
+  }
 }
-
