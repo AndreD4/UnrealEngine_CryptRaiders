a33556f5d1208c4ab8b88275ceb4bc746fd89154
AndreD4 <andremai629@gmail.com>
2023-07-11 13:32:43 +0900

    Waking Physics objects

--- Source/CryptRaiders/Grabber.cpp
+++ Source/CryptRaiders/Grabber.cpp
@@ -41,8 +41,12 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
     return;
   }
 
-  FVector TargetLocation = GetComponentLocation() + GetForwardVector() * HoldDistance;
-  PhysicsHandle->SetTargetLocationAndRotation(TargetLocation, GetComponentRotation());
+  if(PhysicsHandle->GetGrabbedComponent() != nullptr)
+  {
+    FVector TargetLocation = GetComponentLocation() + GetForwardVector() * HoldDistance;
+    PhysicsHandle->SetTargetLocationAndRotation(TargetLocation, GetComponentRotation());
+  }
+  
 }
 
 void UGrabber::Grab()
@@ -71,8 +75,10 @@ void UGrabber::Grab()
 
     if (HasHit)
     {   
+        UPrimitiveComponent* HitComponent = HitResult.GetComponent();
+        HitComponent->WakeAllRigidBodies();
         PhysicsHandle->GrabComponentAtLocationWithRotation(
-          HitResult.GetComponent(),
+          HitComponent,
           NAME_None,
           HitResult.ImpactPoint,
           GetComponentRotation()
@@ -82,7 +88,17 @@ void UGrabber::Grab()
 
 void UGrabber::Release()
 {
-  UE_LOG(LogTemp, Display, TEXT("Released grabber"));
+    UPhysicsHandleComponent* PhysicsHandle = GetPhysicsHandle();
+    if(PhysicsHandle == nullptr)
+    {
+      return;
+    }
+
+    if(PhysicsHandle->GetGrabbedComponent() != nullptr)
+    {   
+        PhysicsHandle->GetGrabbedComponent()->WakeAllRigidBodies();
+        PhysicsHandle->ReleaseComponent();
+    }
 }
 
 UPhysicsHandleComponent* UGrabber::GetPhysicsHandle() const
