0ad06c4c8c0ff7e49ba19add5e5e6775ce05b0ba
AndreD4 <andremai629@gmail.com>
2023-07-14 14:34:51 +0900

    Dependency Injection

--- Source/CryptRaiders/TriggerComponent.cpp
+++ Source/CryptRaiders/TriggerComponent.cpp
@@ -1,11 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "TriggerComponent.h"
 
 UTriggerComponent::UTriggerComponent()
 {
-    PrimaryComponentTick.bCanEverTick = true;
+  PrimaryComponentTick.bCanEverTick = true;
 
   UE_LOG(LogTemp, Display, TEXT("Constructing"));
 }
@@ -13,31 +12,33 @@ UTriggerComponent::UTriggerComponent()
 void UTriggerComponent::BeginPlay()
 {
   Super::BeginPlay();
-  
- 
 }
 
-
-void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
   Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-  AActor* Actor = GetAcceptableActor();
+  AActor *Actor = GetAcceptableActor();
   if (Actor != nullptr)
   {
-    UE_LOG(LogTemp, Display, TEXT("Unlocking"));
+    Mover->SetShouldMove(true);
   }
   else
   {
-    UE_LOG(LogTemp, Display, TEXT("Relocking"));
+    Mover->SetShouldMove(false);
   }
+}
+
+void UTriggerComponent::SetMover(UMover* NewMover)
+{
+  Mover = NewMover;
 }
 
 AActor* UTriggerComponent::GetAcceptableActor() const
 {
-  TArray<AActor*> Actors;
+  TArray<AActor *> Actors;
   GetOverlappingActors(Actors);
-  for (AActor* Actor : Actors)
+  for (AActor *Actor : Actors)
   {
     if (Actor->ActorHasTag(AcceptableActorTag))
     {
--- Source/CryptRaiders/TriggerComponent.h
+++ Source/CryptRaiders/TriggerComponent.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/BoxComponent.h"
+#include "Mover.h"
 #include "TriggerComponent.generated.h"
 
 /**
@@ -21,13 +22,18 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 	
-  public:	
+public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+  UFUNCTION(BlueprintCallable)
+  void SetMover(UMover* Mover);
 
 private:
     UPROPERTY(EditAnywhere)
     FName AcceptableActorTag;
+
+    UMover* Mover;
 
     AActor* GetAcceptableActor() const;
 
